d87b327b4a18e4457176c0a79114bc7716c05c48
runehero <runehero@hotmail.com>
2024-12-15 21:20:17 +0100

    Created new Border and ImageButton derived classes to take over the role of handling TouchBehavior. Also updated pages to use these new classes.

---
+++ MovieFinder/Components/TouchyBorder.xaml.cs
@@ -0,0 +1,16 @@
+namespace MovieFinder.Components;
+
+/// <summary>
+/// A border class that supports <see cref="CommunityToolkit.Maui.Behaviors.TouchBehavior"/>. 
+/// Values are retrieved from application styles.
+/// </summary>
+public partial class TouchyBorder : Border
+{
+	/// <summary>
+	/// Constructor.
+	/// </summary>
+	public TouchyBorder()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MovieFinder/Components/TouchyImageButton.xaml.cs
@@ -0,0 +1,16 @@
+namespace MovieFinder.Components;
+
+/// <summary>
+/// An image button that supports <see cref="CommunityToolkit.Maui.Behaviors.TouchBehavior"/>. 
+/// Values are retrieved from application styles.
+/// </summary>
+public partial class TouchyImageButton : ImageButton
+{
+	/// <summary>
+	/// Constructor.
+	/// </summary>
+	public TouchyImageButton()
+	{
+		InitializeComponent();
+    }
+}
