21bd6837323376dcbc619b6a7d66f971a2312157
runehero <runehero@hotmail.com>
2024-12-16 15:43:28 +0100

    Added a text in watched movies page informing the user that they have not saved any movies.

---
+++ MovieFinder/Converters/InverseCountToBooleanConverter.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+namespace MovieFinder.Converters;
+
+/// <summary>
+/// Returns true for integer numbers less than 1.
+/// </summary>
+public class InverseCountToBooleanConverter : IValueConverter
+{
+    /// <inheritdoc/>
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is int count)
+        {
+            return count <= 0;
+        }
+
+        return false;
+    }
+
+    /// <inheritdoc/>
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
