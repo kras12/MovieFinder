7e90bf3410e876d5bf8316765303c84b557c203c
runehero <runehero@hotmail.com>
2024-12-17 02:16:45 +0100

    Created a loading indicator component that overlays the entire content and locks interaction.

---
+++ MovieFinder/Components/LoadingIndicator.xaml.cs
@@ -0,0 +1,19 @@
+namespace MovieFinder.Components;
+
+/// <summary>
+/// Loading indicator that overlays the whole content and locks interaction. 
+/// </summary>
+public partial class LoadingIndicator : ContentView
+{
+	#region Constructors
+
+	/// <summary>
+	/// Constructor.
+	/// </summary>
+	public LoadingIndicator()
+	{
+		InitializeComponent();
+	}
+
+    #endregion
+}
